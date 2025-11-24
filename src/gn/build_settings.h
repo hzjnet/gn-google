aa52bf39bb2c40dde1af4638bd38bbc9afe0ff95
David 'Digit' Turner <digit@google.com>
2025-11-24 18:13:44 +0100

    WIP: Add `allow_build_dir_symlinks = true` in .gn file.
    Add a new flag in .gn to prevent GN from resolving the build
    directory with realpath(), in order to support the use of symlinks
    in its path. For context see https://fxbug.dev/462499253
    
    Change-Id: I905d2d71c7f62918630a2abd9d60e09e76f1eee5

--- src/gn/build_settings.h
+++ src/gn/build_settings.h
@@ -77,6 +77,15 @@ class BuildSettings {
     no_stamp_files_ = no_stamp_files;
   }
 
+  // The 'allow_build_dir_symlinks' boolean flag can be set to allow symlinks
+  // in the specification of the build directory when running GN commands.
+  // The default is to call realpath() to obtain the absolute path of the build
+  // directory, then computing a source root-relative path from it.
+  bool allow_build_dir_symlinks() const { return allow_build_dir_symlinks_; }
+  void set_allow_build_dir_symlinks(bool allow_build_dir_symlinks) {
+    allow_build_dir_symlinks_ = allow_build_dir_symlinks;
+  }
+
   const SourceFile& build_config_file() const { return build_config_file_; }
   void set_build_config_file(const SourceFile& f) { build_config_file_ = f; }
 
@@ -153,6 +162,7 @@ class BuildSettings {
   // See 40045b9 for the reason behind using 1.7.2 as the default version.
   Version ninja_required_version_{1, 7, 2};
   bool no_stamp_files_ = true;
+  bool allow_build_dir_symlinks_ = false;
 
   SourceFile build_config_file_;
   SourceFile arg_file_template_path_;
