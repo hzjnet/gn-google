c346fd1f19ef9b890da692bb5f39b953b2842202
Neri Marschik <nerima@google.com>
2026-02-04 18:47:57 +0900

    Include validations when collecting metadata
    Some Fuchsia tests depend on metadata to collect clippy output. This
    failed because validations were not considered when collecting metadata.
    
    Bug: 478798763
    Change-Id: Id213821800144a14c8113f13caf7833748679f0c

--- src/gn/target_unittest.cc
+++ src/gn/target_unittest.cc
@@ -1538,3 +1538,38 @@ TEST_F(TargetTest, ModuleMap) {
   ASSERT_EQ(1u, output.size());
   EXPECT_EQ("input.modulemap.pcm", output[0].value()) << output[0].value();
 }
+
+TEST(TargetTest, CollectMetadataWithValidation) {
+  TestWithScope setup;
+
+  TestTarget a(setup, "//foo:a", Target::SOURCE_SET);
+  Value a_expected(nullptr, Value::LIST);
+  a_expected.list_value().push_back(Value(nullptr, "foo"));
+  a.metadata().contents().insert(
+      std::pair<std::string_view, Value>("walk", a_expected));
+
+  TestTarget b(setup, "//foo:b", Target::SOURCE_SET);
+  Value b_expected(nullptr, Value::LIST);
+  b_expected.list_value().push_back(Value(nullptr, "bar"));
+  b.metadata().contents().insert(
+      std::pair<std::string_view, Value>("walk", b_expected));
+
+  a.validations().push_back(LabelTargetPair(&b));
+
+  std::vector<std::string> data_keys;
+  data_keys.push_back("walk");
+
+  std::vector<std::string> walk_keys;
+
+  Err err;
+  std::vector<Value> result;
+  TargetSet targets;
+  a.GetMetadata(data_keys, walk_keys, SourceDir(), false, &result, &targets,
+                &err);
+  EXPECT_FALSE(err.has_error());
+
+  std::vector<Value> expected;
+  expected.push_back(Value(nullptr, "bar"));
+  expected.push_back(Value(nullptr, "foo"));
+  EXPECT_EQ(result, expected);
+}
