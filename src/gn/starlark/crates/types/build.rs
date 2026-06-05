8d35b83847d1bf61bad0b8176a8aab6afc052ae1
Matt Stark <msta@google.com>
2026-06-05 14:22:05 +1000

    Fix issues where cargo wouldn't rerun after static libraries were updated.
    This adds `cargo:rerun_if_changed` to the static libraries to ensure
    they're up to date. Apparently cargo:rustc-link-lib is insufficient.
    
    Bug: 528225104
    Change-Id: I258ce5190c8d181e3bd361a36370f51a6a6a6964
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/23882
    Reviewed-by: Takuto Ikuta <tikuta@google.com>
    Commit-Queue: Matt Stark <msta@google.com>

--- src/gn/starlark/crates/types/build.rs
+++ src/gn/starlark/crates/types/build.rs
@@ -2,16 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+include!("../build_helper.rs");
+
 fn main() {
-    // When running with ninja, it should set NINJA_OUT_DIR.
-    // If running directly with cargo, we know nothing about the output directory,
-    // so we fall back to assuming the default one.
-    let out_dir = if let Ok(out_dir) = std::env::var("NINJA_OUT_DIR") {
-        std::path::PathBuf::from(out_dir)
-    } else {
-        let manifest_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-        manifest_dir.join("../../../../../out")
-    };
-    println!("cargo:rustc-link-search=native={}", out_dir.display());
-    println!("cargo:rustc-link-lib=static=string_atom");
+    require_libs(&["string_atom"]);
 }
