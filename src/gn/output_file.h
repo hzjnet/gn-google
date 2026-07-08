10d3ab4387f7f4ad0d3fb9d626218a6e09e71d86
msta <msta@google.com>
2026-07-08 12:52:12 +1000

    Replace OutputFile with StringAtom under the hood
    The purpose of this is to allow OutputFile to be cheaply copied.
    
    Bug: 491925153
    Change-Id: I4d42b77f31a0608f978712143d0c6e746a6a6964
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/23921
    Reviewed-by: Takuto Ikuta <tikuta@google.com>
    Commit-Queue: Matt Stark <msta@google.com>

--- src/gn/output_file.h
+++ src/gn/output_file.h
@@ -8,32 +8,25 @@
 #include <stddef.h>
 
 #include <string_view>
-#include <vector>
+
+#include "gn/string_atom.h"
 
 class BuildSettings;
 class SourceDir;
 class SourceFile;
 
-// A simple wrapper around a vector of chars that indicates the path
+// A simple wrapper around StringAtom that indicates the path
 // relative to the output directory.
 class OutputFile {
  public:
   OutputFile() = default;
 
-  explicit OutputFile(std::string_view v) { value_.assign(v.begin(), v.end()); }
+  explicit OutputFile(std::string_view v) : value_(v) {}
 
   OutputFile(const BuildSettings* build_settings,
              const SourceFile& source_file);
 
-  std::string_view value() const {
-    return std::string_view(value_.data(), value_.size());
-  }
-
-  void resize(std::size_t n) { value_.resize(n); }
-
-  void append(std::string_view v) {
-    value_.insert(value_.end(), v.begin(), v.end());
-  }
+  std::string_view value() const { return value_; }
 
   // Converts to a SourceFile by prepending the build directory to the file.
   // The *Dir version requires that the current OutputFile ends in a slash, and
@@ -42,18 +35,10 @@ class OutputFile {
   SourceDir AsSourceDir(const BuildSettings* build_settings) const;
 
   bool operator==(const OutputFile& other) const = default;
-  bool operator!=(const OutputFile& other) const = default;
-  bool operator<(const OutputFile& other) const = default;
   std::strong_ordering operator<=>(const OutputFile& other) const = default;
 
  private:
-  // Storing this as a vector<char> instead of a std::string has some tradeoffs.
-  // * When OutputFile is moved (eg. vector<OutputFile>.push_back), string_views
-  //   pointing to OutputFile stay valid
-  // * We now lose small string optimization. This is probably fine, and may in
-  //   fact even be an improvement, as OutputFiles are almost universally very
-  //   long and thus this may help with branch prediction.
-  std::vector<char> value_;
+  StringAtom value_;
 };
 
 namespace std {
